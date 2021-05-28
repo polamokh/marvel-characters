ac3089f386681e66d5a3181cf1cf8956203eb20c
Pola <pola.mikhail@engineer.com>
2021-05-28 16:20:09 +0200

    Add splash screen

--- app/src/main/java/me/polamokh/marvelcharacters/ui/MainActivity.kt
+++ app/src/main/java/me/polamokh/marvelcharacters/ui/MainActivity.kt
@@ -1,13 +1,14 @@
 package me.polamokh.marvelcharacters.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import me.polamokh.marvelcharacters.R
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_MarvelCharacters)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
