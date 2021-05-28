c95c00764c2e84e1da2bbfe45bb0ae5e24f32f39
Pola <pola.mikhail@engineer.com>
2021-05-28 19:27:11 +0200

    Add search view to toolbar

--- app/src/main/java/me/polamokh/marvelcharacters/ui/characters/CharactersFragment.kt
+++ app/src/main/java/me/polamokh/marvelcharacters/ui/characters/CharactersFragment.kt
@@ -1,12 +1,12 @@
 package me.polamokh.marvelcharacters.ui.characters
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import me.polamokh.marvelcharacters.R
 import me.polamokh.marvelcharacters.adapters.CharactersAdapter
 import me.polamokh.marvelcharacters.databinding.FragmentCharactersBinding
 
@@ -20,6 +20,8 @@ class CharactersFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        setHasOptionsMenu(true)
+
         binding = FragmentCharactersBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -35,5 +37,22 @@ class CharactersFragment : Fragment() {
         viewModel.marvelCharacters.observe(viewLifecycleOwner) {
             charactersAdapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_search, menu)
+
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //TODO: Search for a character
+                return true
+            }
+        })
     }
 }
