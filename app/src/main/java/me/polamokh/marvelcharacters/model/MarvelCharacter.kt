a63a6b795b7f206e3b4e0ba2b38a20cb1d7459f8
Pola <pola.mikhail@engineer.com>
2021-05-29 18:02:00 +0200

    Remove marvel character unused properties

--- app/src/main/java/me/polamokh/marvelcharacters/model/MarvelCharacter.kt
+++ app/src/main/java/me/polamokh/marvelcharacters/model/MarvelCharacter.kt
@@ -8,9 +8,5 @@ data class MarvelCharacter(
     val name: String,
     val description: String,
     val thumbnail: Thumbnail,
-    val comics: Spotlight,
-    val stories: Spotlight,
-    val events: Spotlight,
-    val series: Spotlight,
     val id: Int,
 ) : Parcelable
--- app/src/main/java/me/polamokh/marvelcharacters/model/Spotlight.kt
+++
@@ -1,9 +0,0 @@
-package me.polamokh.marvelcharacters.model
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class Spotlight(
-    val items: List<SpotlightResource>
-) : Parcelable
